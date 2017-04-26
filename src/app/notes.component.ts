139fe23e7e6fdd08b084145a1de0dc828034ea12
Rémy Taymans <remytaymans@gmail.com>
2017-04-26 13:06:42 +0200

    Create form for the notes

--- src/app/notes.component.ts
+++ src/app/notes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { Note } from './note';
 import { NOTES } from './mock-notes';
@@ -10,5 +10,26 @@ import { NOTES } from './mock-notes';
 
 export class NotesComponent  { 
   title = 'Notepad';
+
+  //@Input() notes = NOTES;
   notes = NOTES;
+  note_edited = -1;
+  new_note: Note = null;
+
+  validate(note: Note) {
+    console.log(note);
+    this.note_edited = -1;
+  }
+
+  post_note(note: Note) {
+    console.log(note);
+  }
+
+  init_new_note() {
+    this.new_note = new Note();
+  }
+
+  del_new_note() {
+    this.new_note = null;
+  }
 }
